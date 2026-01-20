ee334cd8b9cc73dfd1e15c94ddfdf855aca823fa
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-20 19:21:38 -0300

    Feat: Add Global Product Search and Categories API

--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -7,6 +7,10 @@ const productController = require('./product.controller');
 // Create product for a specific client
 router.post('/clients/:clientId/products', productController.create);
 router.post('/clients/:clientId/products/bulk-import', productController.bulkImport);
+// Product utilities
+router.get('/products/global-search', productController.globalSearch);
+router.get('/products/categories', productController.getCategories);
+
 // Get products by client slug (public)
 router.get('/clients/:clientSlug/products', productController.getByClientSlug);
 
