d664c60610c1a082fa89b6d2f2b5dcf164bb716b
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-22 11:34:40 -0300

    fix: correctly register ProductInteraction model

--- src/models/ProductInteraction.js
+++ src/models/ProductInteraction.js
@@ -1,35 +1,36 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('./index').sequelize;
 
-const ProductInteraction = sequelize.define('ProductInteraction', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    productId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    type: {
-        type: DataTypes.STRING, // view, favorite, nutrition
-        allowNull: false
-    },
-    ipAddress: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    userAgent: {
-        type: DataTypes.TEXT,
-        allowNull: true
-    },
-    createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
-    }
-}, {
-    tableName: 'product_interactions',
-    updatedAt: false // Only insert log
-});
+module.exports = (sequelize) => {
+    const ProductInteraction = sequelize.define('ProductInteraction', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        productId: {
+            type: DataTypes.UUID, // Changed to UUID to match Product model's id type
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.STRING, // view, favorite, nutrition
+            allowNull: false
+        },
+        ipAddress: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        userAgent: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        tableName: 'product_interactions',
+        updatedAt: false // Only insert log
+    });
 
-module.exports = ProductInteraction;
+    return ProductInteraction;
+};
