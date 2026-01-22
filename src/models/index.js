6bbfe1b58fd9c367be6ca9cdb1e22314257a8e53
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-22 11:35:12 -0300

    fix: correctly register ProductInteraction model (retry)

--- src/models/index.js
+++ src/models/index.js
@@ -14,8 +14,10 @@ db.Product = require('./Product')(sequelize);
 db.Category = require('./Category')(sequelize);
 db.User = require('./User')(sequelize);
 db.ClientFile = require('./ClientFile')(sequelize);
+db.ProductInteraction = require('./ProductInteraction')(sequelize);
 
 // Associations
+// Client <-> Products
 db.Client.hasMany(db.Product, { foreignKey: 'clientId', as: 'products' });
 db.Product.belongsTo(db.Client, { foreignKey: 'clientId', as: 'client' });
 
@@ -24,5 +26,9 @@ db.Category.belongsTo(db.Client, { foreignKey: 'clientId', as: 'client' });
 
 db.Client.hasMany(db.ClientFile, { foreignKey: 'clientId', as: 'files' });
 db.ClientFile.belongsTo(db.Client, { foreignKey: 'clientId', as: 'client' });
+
+// Product <-> Interactions
+db.Product.hasMany(db.ProductInteraction, { foreignKey: 'productId', as: 'interactions' });
+db.ProductInteraction.belongsTo(db.Product, { foreignKey: 'productId', as: 'product' });
 
 module.exports = db;
